7efc4a512f2b54b406f6edc85e3cbc2d7d4bfe6f
hangnt <hangnt3@vmogroup.com>
2023-12-29 16:27:44 +0700

    fix(frontend): router

--- src/pages/energy/[energyId]/edit.tsx
+++ src/pages/energy/[energyId]/edit.tsx
@@ -120,7 +120,7 @@ export default function UpdateEnergy() {
           typeAlert: "success",
         })
       );
-      // router.push(`/energy/${router?.query?.energyId}`);
+      router.push(`/energy/${router?.query?.energyId}`);
     }
   };
   return (
